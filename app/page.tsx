ce364970c2c605c68a7d3de4ed2ef616eed1207a
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2023-10-03 20:52:39 +0630

    so far

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,54 @@ export default function Home() {
   const projects = [
     {
       id:1,
-      link:'https://sdec78451.vercel.app',
-      text:'Table'
+      link:'https://21magic.vercel.app',
+      text:'Magic',
+      size:4,
     },
     {
       id:2,
       link:'https://fitwaygym.vercel.app',
-      text:'Gym'
+      text:'Gym',
+      size:4,
     },
     {
       id:3,
       link:'https://meddusa.vercel.app',
-      text:'Medical'
-    }
+      text:'Med',
+      size:4,
+    },
+    {
+      id:4,
+      link:'https://sdec78451.vercel.app',
+      text:'Table',
+      size:4,
+    },
+    {
+      id:5,
+      link:'https://quiz.zsphinx.repl.co/',
+      text:'Quiz',
+      size:3,
+    },
+    {
+      id:6,
+      link:'https://color.zsphinx.repl.co/',
+      text:'Color',
+      size:2
+    },
+    {
+      id:7,
+      link:'https://randomuser.zsphinx.repl.co/',
+      text:'JSON',
+      size:2
+    },
   ]
   return (
-    <main className="flex gap-3 w-full h-full justify-center">
+    <main className="flex gap-5 h-full justify-center flex-wrap w-[70%]">
       {
         projects.map(i => (
           <a key={i.id} href={i.link} target="blank">
-            <div className="min-w-10 min-h-10 max-w-2xl max-h-2xl bg-[#2f2f2f] p-10 rounded-xl shadow-md shadow-stone-900 hover:bg-[#3a3a3a] transition">
-                <p className="text-xl font-bold text-[#aaa]">{i.text}</p>
+            <div className="group min-w-[100px] min-h-[100px] bg-[#2f2f2f] p-5 rounded-xl shadow-md shadow-stone-900 hover:bg-[#3a3a3a] border-b-stone-900 hover:border-b-green-500 border-b-2 transition flex items-center justify-center">
+                <p className="leading-0 font-bold text-[#aaa] drop-shadow-2xl transition group-hover:text-green-200" style={{fontSize: i.size*6}}>{i.text}</p>
             </div>
           </a>
         ))

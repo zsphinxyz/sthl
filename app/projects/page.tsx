20c0dfd003240cac7d0ed091447a0fb323153835
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2024-05-29 08:45:39 +0630

    update

---
+++ app/projects/page.tsx
@@ -0,0 +1,23 @@
+
+const projects = [
+    'zjpp.vercel.app',
+    'sdec78451.vercel.app/list',
+    'soe-thiha-lwin-resume.vercel.app',
+    'z3d.vercel.app',
+    'zfirst-z-sphinxs-projects.vercel.app',
+    'zauthtest.vercel.app',
+    'velolev.vercel.app',
+
+]
+
+export default function page() {
+  return (
+    <main className="text-white flex flex-col gap-2 ">
+        {
+            projects.map( i => (
+                <a href={`https://${i}`} target="_blank" className="text-cyan-500 underline hover:no-underline visited:text-purple-400">{i}</a>
+            ))
+        }
+    </main>
+  )
+}
